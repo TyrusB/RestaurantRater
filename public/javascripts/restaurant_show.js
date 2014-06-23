48b4a522bf48758110f18ed5bd33ebbbcfd0f5ce
TyrusB <brooks85.ty@gmail.com>
2014-06-23 01:53:13 -0700

    Add current rating to ratings slider

--- public/javascripts/restaurant_show.js
+++ public/javascripts/restaurant_show.js
@@ -1,7 +1,22 @@
 $(function() {
   var restaurant = JSON.parse($('#restaurant-data').html());
 
-  $('.slider').slider({step: 10});
+  $('.slider').slider({
+    step: 10, 
+    value: 50,
+    change: function(event, ui) {
+      var newRating = ui.value / 20;
+      $('.current-rating-num').text(newRating);
+
+      if (newRating < 2) {
+        $('.current-rating-num').css('color', 'red');
+      } else if (newRating >= 3.5) {
+        $('.current-rating-num').css('color', 'green');
+      } else {
+        $('.current-rating-num').css('color', 'gold');
+      }
+    }
+  });
 
   $('#submit-ratings').on('click', function(event) {
     event.preventDefault();
